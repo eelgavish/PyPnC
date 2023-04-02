64cc94edd6bd5a43a73563de141b591e09eb22f2
kevt19 <kevt19@utexas.edu>
2023-04-01 20:37:08 -0500

    Implemented Get Height/dh_dy/dh_dx functions in Travis Class

--- pnc/planner/locomotion/towr_plus/include/towr_plus/terrain/examples/height_map_examples.h
+++ pnc/planner/locomotion/towr_plus/include/towr_plus/terrain/examples/height_map_examples.h
@@ -36,7 +36,16 @@ Modified by Junhyeok Ahn (junhyeokahn91@gmail.com) for towr+
 
 #include <towr_plus/terrain/height_map.h>
 #include <iostream>
-#include <jpeglib.h>
+#include <cmath>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <iomanip>
+#include <vector>
+#include <iterator>
+#include <cstdlib>
+
 
 namespace towr_plus {
 
@@ -177,75 +186,408 @@ private:
 /**
  * @brief Terrain from height map file.
  */
-class Kierran : public HeightMap {
+class Travis : public HeightMap 
+{
+public:
+
+  ////////// Class Attributes ///////////////////////////////////
+
+  // Vectors containing X and Y positions along with height Data (Z)
+  std::vector<float> xpos;
+  std::vector<float> ypos;
+  std::vector<float> zpos;
+
+  // Variable that outputs size of vectors containing x,y, and z.
+  int data_vector_size;
+
+  // Variable that outputs point resolution
+  double resolution; 
+
+  //////////////////////////////////////////////////////////////
+
+
+
+
+  //////// CONSTRUCTOR //////////////////////////////////////////
+
+  // Takes in a string file path to the .txt file containing height data of a height map
+  Travis(std::string data_filename) 
+  {
+    std::fstream mapfile;
+    mapfile.open(data_filename, std::ios::in);
+
+
+    std::string line;
+    std::string number_as_string;
+
+    while (std::getline(mapfile, line, '\n'))
+    {
+            
+        int i = 0;
+
+        for (std::istringstream ss(line); std::getline(ss, number_as_string, ',');)
+        {
+          if (i == 0)
+          {
+            xpos.push_back(std::stof(number_as_string));
+          }
+          else if (i == 1)
+          {
+            ypos.push_back(std::stof(number_as_string));
+          }
+          else if (i == 2)
+          {
+            zpos.push_back(std::stof(number_as_string));
+          }
+                
+                
+          i++;
+        }
+
+              
+    }
+
+    data_vector_size = xpos.size();
+    resolution = ypos[1] - ypos[0];
+    
+
+    ///// Uncomment the following block of code for debugging purposes /////
+
+    // std::cout << "X Position: " ;
+    // for (int i = 0; i < xpos.size(); i++ )
+    // {
+    //   std::cout << xpos[i] << ", ";
+    // }
+
+    // std::cout << "\nY position: ";
+
+    // for (int i = 0; i < ypos.size(); i++ )
+    // {
+    //   std::cout << ypos[i] << ", ";
+    // }
+
+    // std::cout << "\nZ position: ";
+    // for (int i = 0; i < zpos.size(); i++ )
+    // {
+    //   std::cout << zpos[i] << ", ";
+    // }
+    // std::cout << "\n";
+
+    mapfile.close();
+
+    };
+    //////////////////////////////////////////////////////////
+
+
+
+
+
+    ///////////////////// Class Methods /////////////////////
+
+   double GetHeight(double x, double y) const override 
+   {
+    // Converting Meter Space coordinates to Index Space
+    double height;
+    double x_index = x/resolution;
+    double y_index = y/resolution;
+
+
+    // Obtain the Whole part and Decimal part of Index Space Coordinates
+    double x_whole;
+    double x_fractional;
+
+    double y_whole;
+    double y_fractional;
+
+    x_fractional = std::modf(x_index, &x_whole);
+    y_fractional = std::modf(y_index, &y_whole);
+
+    // Index Space Coordinates of surrounding points (P1)
+    int p1_x = x_whole;
+    int p1_y = y_whole;
+    int p1_z = p1_x*100 + p1_y;
+
+    // Index Space Coordinates of surrounding points (P2)
+    int p2_x = x_whole + 1;
+    int p2_y = y_whole;
+    int p2_z = p2_x*100 + p2_y;
+
+    // Index Space Coordinates of surrounding points (P3)
+    int p3_x = x_whole;
+    int p3_y = y_whole + 1;
+    int p3_z = p3_x*100 + p3_y;
+
+    // Index Space Coordinates of surrounding points (P4)
+    int p4_x = x_whole + 1;
+    int p4_y = y_whole + 1;
+    int p4_z = p4_x*100 + p4_y;
+
+    // Respective Heights (Z value in meters) for surrounding points
+    double p1_height = zpos[p1_z];
+    double p2_height = zpos[p2_z];
+    double p3_height = zpos[p3_z];
+    double p4_height = zpos[p4_z];
+
+
+    // Height of desired point along the Y-Direction
+    
+    //Bottom Section
+    double delta_h_bottom = fabs(p1_height - p2_height);
+    double delta_x_bottom = x_fractional*resolution;
+    double delta_z_bottom = delta_h_bottom*delta_x_bottom;
+    double h1_bottom;
+    if (p1_height > p2_height)
+    {
+      h1_bottom = p1_height - delta_z_bottom;
+    }
+    else
+    {
+      h1_bottom = p2_height - delta_z_bottom;
+    }
+
+
+    //Top Section
+    double delta_h_top = fabs(p3_height - p4_height);
+    double delta_x_top = x_fractional*resolution;
+    double delta_z_top = delta_h_bottom*delta_x_bottom;
+    double h2_top;
+    if (p3_height > p4_height)
+    {
+      h2_top = p3_height - delta_z_top;
+    }
+    else
+    {
+      h2_top = p4_height - delta_z_top;
+    }
+
+
+    //Desired Height Section
+    double delta_h_des = fabs(h1_bottom - h2_top);
+    double delta_y_des = y_fractional*resolution;
+    double delta_z_des = delta_h_des*delta_y_des;
+    if (h1_bottom > h2_top)
+    {
+      height = h1_bottom - delta_z_des;
+    }
+    else
+    {
+      height = h2_top - delta_z_des;
+    }
+
+
+
+    ///// Uncomment the following block of code for debugging purposes /////
+
+        // std::cout << "Desired X and Y Coordinates: " << x << ", " << y << std::endl;
+        // std::cout << "\n" << std::endl;
+
+
+        // std::cout << "X Whole: " << x_whole << std::endl;
+        // std::cout << "X Fraction: " << x_fractional << std::endl;
+        // std::cout << "Y Whole: " << y_whole << std::endl;
+        // std::cout << "Y Fraction: " << y_fractional << std::endl;
+        // std::cout << "\n" << std::endl;
+
+        // std::cout << "1st Point Height: " << p1_height << std::endl;
+        // std::cout << "2nd Point Height: " << p2_height << std::endl;
+        // std::cout << "3rd Point Height: " << p3_height << std::endl;
+        // std::cout << "4th Pount Height: " << p4_height << std::endl;
+        // std::cout << "\n" << std::endl;
+
+        // std::cout << "delta_h_bottom: " << delta_h_bottom << std::endl;
+        // std::cout << "delta_x_bottom: " << delta_x_bottom << std::endl;
+        // std::cout << "delta_z_bottom: " << delta_z_bottom << std::endl;
+        // std::cout << "h1_bottom: " << h1_bottom << std::endl;
+        // std::cout << "\n" << std::endl;
+
+        // std::cout << "delta_h_top: " << delta_h_top << std::endl;
+        // std::cout << "delta_x_top: " << delta_x_top << std::endl;
+        // std::cout << "delta_z_top: " << delta_z_top << std::endl;
+        // std::cout << "h2_top: " << h2_top << std::endl;
+        // std::cout << "\n" << std::endl;
+        
+        // std::cout << "delta_h_des: " << delta_h_des << std::endl;
+        // std::cout << "delta__y_des: " << delta_y_des << std::endl;
+        // std::cout << "delta_z_des: " << delta_z_des << std::endl;
+        // std::cout << "h3_des: " << h3_des << std::endl;
+        // std::cout << "\n" << std::endl;
+    return height;
+   }
+
+
+   double GetDerivWrtY(double x, double y) const override
+   {
+    // Converting Meter Space coordinates to Index Space
+    double dh_dy;
+    double x_index = x/resolution;
+    double y_index = y/resolution;
+
+
+    // Obtain the Whole part and Decimal part of Index Space Coordinates
+    double x_whole;
+    double x_fractional;
+
+    double y_whole;
+    double y_fractional;
+
+    x_fractional = std::modf(x_index, &x_whole);
+    y_fractional = std::modf(y_index, &y_whole);
+
+    // Index Space Coordinates of surrounding points (P1)
+    int p1_x = x_whole;
+    int p1_y = y_whole;
+    int p1_z = p1_x*100 + p1_y;
+
+    // Index Space Coordinates of surrounding points (P2)
+    int p2_x = x_whole + 1;
+    int p2_y = y_whole;
+    int p2_z = p2_x*100 + p2_y;
+
+    // Index Space Coordinates of surrounding points (P3)
+    int p3_x = x_whole;
+    int p3_y = y_whole + 1;
+    int p3_z = p3_x*100 + p3_y;
+
+    // Index Space Coordinates of surrounding points (P4)
+    int p4_x = x_whole + 1;
+    int p4_y = y_whole + 1;
+    int p4_z = p4_x*100 + p4_y;
+
+    // Respective Heights (Z value in meters) for surrounding points
+    double p1_height = zpos[p1_z];
+    double p2_height = zpos[p2_z];
+    double p3_height = zpos[p3_z];
+    double p4_height = zpos[p4_z];
+
+
+    // Height of desired point along the Y-Direction
+    
+    //Bottom Section
+    double delta_h_bottom = fabs(p1_height - p2_height);
+    double delta_x_bottom = x_fractional*resolution;
+    double delta_z_bottom = delta_h_bottom*delta_x_bottom;
+    double h1_bottom;
+    if (p1_height > p2_height)
+    {
+      h1_bottom = p1_height - delta_z_bottom;
+    }
+    else
+    {
+      h1_bottom = p2_height - delta_z_bottom;
+    }
+
+
+    //Top Section
+    double delta_h_top = fabs(p3_height - p4_height);
+    double delta_x_top = x_fractional*resolution;
+    double delta_z_top = delta_h_bottom*delta_x_bottom;
+    double h2_top;
+    if (p3_height > p4_height)
+    {
+      h2_top = p3_height - delta_z_top;
+    }
+    else
+    {
+      h2_top = p4_height - delta_z_top;
+    }
+
+
+    //Derivative of Height Wrt to Y
+    dh_dy =  (h2_top - h1_bottom) / resolution; // if h2_top > h1_bottom, then Positive slope. Else, Negative Slope
+
+    return dh_dy;
+   }
+
+
+   double GetDerivWrtX(double x, double y) const override
+   {
+    // Converting Meter Space coordinates to Index Space
+    double dh_dx;
+    double x_index = x/resolution;
+    double y_index = y/resolution;
+
+
+    // Obtain the Whole part and Decimal part of Index Space Coordinates
+    double x_whole;
+    double x_fractional;
+
+    double y_whole;
+    double y_fractional;
+
+    x_fractional = std::modf(x_index, &x_whole);
+    y_fractional = std::modf(y_index, &y_whole);
+
+    // Index Space Coordinates of surrounding points (P1)
+    int p1_x = x_whole;
+    int p1_y = y_whole;
+    int p1_z = p1_x*100 + p1_y;
+
+    // Index Space Coordinates of surrounding points (P2)
+    int p2_x = x_whole + 1;
+    int p2_y = y_whole;
+    int p2_z = p2_x*100 + p2_y;
+
+    // Index Space Coordinates of surrounding points (P3)
+    int p3_x = x_whole;
+    int p3_y = y_whole + 1;
+    int p3_z = p3_x*100 + p3_y;
+
+    // Index Space Coordinates of surrounding points (P4)
+    int p4_x = x_whole + 1;
+    int p4_y = y_whole + 1;
+    int p4_z = p4_x*100 + p4_y;
+
+    // Respective Heights (Z value in meters) for surrounding points
+    double p1_height = zpos[p1_z];
+    double p2_height = zpos[p2_z];
+    double p3_height = zpos[p3_z];
+    double p4_height = zpos[p4_z];
+
+
+    // Height of desired point along the Y-Direction
+    
+    //Bottom Section
+    double delta_h_left = fabs(p1_height - p3_height);
+    double delta_y_left = y_fractional*resolution;
+    double delta_z_left = delta_h_left*delta_y_left;
+    double h1_left;
+    if (p1_height > p3_height)
+    {
+      h1_left = p1_height - delta_z_left;
+    }
+    else
+    {
+      h1_left = p3_height - delta_z_left;
+    }
+
+
+    //Top Section
+    double delta_h_right = fabs(p2_height - p4_height);
+    double delta_y_right = y_fractional*resolution;
+    double delta_z_right = delta_h_right*delta_y_right;
+    double h2_right;
+    if (p2_height > p4_height)
+    {
+      h2_right = p2_height - delta_z_right;
+    }
+    else
+    {
+      h2_right = p4_height - delta_z_right;
+    }
+
+
+    //Derivative of Height Wrt to Y
+    dh_dx =  (h2_right - h1_left) / resolution; // if h2_top > h1_bottom, then Positive slope. Else, Negative Slope
+
+    return dh_dx;
+   }
+
+
+    /////////////////////////////////////////////////////////
 
 private:
-  int width;
-  int height;    
-  double xscale = .05;
-  double yscale = .05;
-  double zscale = .01;  
-  double zero;
-  unsigned char* data;
+}    
 
-public:
-  Kierran(const char* filename){
-    // Open the input file
-    FILE* infile = fopen(filename, "rb");
-    if (infile == NULL) {
-        std::cerr << "Error: failed to open file " << filename << std::endl;
-        exit(1);
-    }
-
-    // Initialize the JPEG decompression object
-    struct jpeg_decompress_struct cinfo;
-    struct jpeg_error_mgr jerr;
-
-    cinfo.err = jpeg_std_error(&jerr);
-    jpeg_create_decompress(&cinfo);
-    jpeg_stdio_src(&cinfo, infile);
-    jpeg_read_header(&cinfo, TRUE);
-    jpeg_start_decompress(&cinfo);
-
-    // Allocate memory for the height map data
-    width = cinfo.output_width;
-    height = cinfo.output_height;
-    data = new unsigned char[width * height];
-
-    // Read the height map data
-    JSAMPROW row_pointer;
-    int row_stride = cinfo.output_width * cinfo.output_components;
-
-    while (cinfo.output_scanline < cinfo.output_height) {
-        row_pointer = &data[cinfo.output_scanline * width];
-        jpeg_read_scanlines(&cinfo, &row_pointer, 1);
-    }
-
-    // Clean up
-    jpeg_finish_decompress(&cinfo);
-    jpeg_destroy_decompress(&cinfo);
-    fclose(infile);
-
-    const int length = int(width*height);
-    for(int i = 0; i < length; i++){
-      data[i] *= zscale;
-      std::cout << data[i];
-    } 
-    zero = data[(int)(length/2)];
-
-  }
-
-  ~Kierran() {
-    delete[] data;
-  }
-
-  int getWidth() const;
-  int getHeight() const;
-
-  double GetHeight(double x, double y) const override;
-  double GetHeightDerivWrtX(double x, double y) const override;
-  double GetHeightDerivWrtY(double x, double y) const override;
-};
 
 /** @}*/
 
